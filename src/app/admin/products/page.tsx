7c661089086b8deb43c06f527119e10d0ff94178
Ray Mak <projectraymak@gmail.com>
2024-07-25 15:25:26 -0700

    Add add new product logic

---
+++ src/app/admin/products/page.tsx
@@ -0,0 +1,58 @@
+import { Button } from "@/components/ui/button"
+import { PageHeader } from "../_components/PageHeader"
+import Link from "next/link"
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import db from "@/db/db"
+
+export default function AdminProductsPage() {
+  return (
+    <>
+      <div className="flex justify-between items-center gap-4">
+        <PageHeader>Products</PageHeader>
+        <Button asChild>
+          <Link href="/admin/products/new">Add Product</Link>
+        </Button>
+      </div>
+      <ProductsTable />
+    </>
+  )
+}
+
+async function ProductsTable() {
+  const products = await db.product.findMany({
+    select: {
+      id: true,
+      name: true,
+      priceInCents: true,
+      isAvailableForPurchase: true,
+      _count: { select: { orders: true } },
+    },
+    orderBy: { name: "asc" },
+  })
+
+  if (products.length === 0) return <p>No products found</p>
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-0">
+            <span className="sr-only">Available For Purchase</span>
+          </TableHead>
+          <TableHead>Name</TableHead>
+          <TableHead>Price</TableHead>
+          <TableHead>Orders</TableHead>
+          <TableHead className="w-0">
+            <span className="sr-only">Actions</span>
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody></TableBody>
+    </Table>
+  )
+}
